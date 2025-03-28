5aecf06cf09456fad29e606fad9d954c770f115e
Dan <danci1159@gmail.com>
2025-03-28 16:20:46 +0100

    feat: download snapshots

--- app/components/Webcam.vue
+++ app/components/Webcam.vue
@@ -1,18 +1,22 @@
 <script lang="ts" setup>
-import { ref, onBeforeUnmount } from 'vue';
+import {ref, onBeforeUnmount} from 'vue';
+import JSZip from 'jszip';
+import {saveAs} from 'file-saver';
 
 const videoRef = ref<HTMLVideoElement | null>(null);
 const canvasRef = ref<HTMLCanvasElement | null>(null);
 const snapshots = ref<string[]>([]);
 const isWebcamActive = ref<boolean>(false);
 
+const videoWidth = 1920;
+const videoHeight = 1080;
+
 const startWebcam = async (): Promise<void> => {
   try {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    const stream = await navigator.mediaDevices.getUserMedia({video: {width: videoWidth, height: videoHeight}});
     if (videoRef.value) {
       videoRef.value.srcObject = stream;
       isWebcamActive.value = true;
-      console.log('Webcam started:', isWebcamActive.value); // Check if the webcam starts
     }
   } catch (error) {
     console.error('Error accessing webcam:', error);
@@ -44,15 +48,29 @@ const takeSnapshot = (): void => {
   }
 };
 
+const downloadAllSnapshots = async (): Promise<void> => {
+  const zip = new JSZip();
+
+  snapshots.value.forEach((img, index) => {
+    const imgData = img.split(',')[1];
+    zip.file(`snapshot${index + 1}.png`, imgData, {base64: true});
+  });
+
+  zip.generateAsync({type: 'blob'}).then(content => {
+    saveAs(content, 'snapshots.zip');
+  });
+};
+
 onBeforeUnmount(stopWebcam);
 </script>
 
 <template>
   <div class="flex flex-col items-center p-4 space-y-4">
 
-    <video v-show="isWebcamActive" ref="videoRef" autoplay class="border rounded-lg shadow-lg w-full max-w-[640px] h-auto object-cover"/>
+    <video v-show="isWebcamActive" ref="videoRef" autoplay
+           class="border rounded-lg shadow-lg w-full max-w-[640px] h-auto object-cover"/>
     <div v-if="!isWebcamActive" class="grid gap-2">
-      <USkeleton class="w-[640px] h-[480px]" />
+      <USkeleton class="w-[640px] h-[480px]"/>
     </div>
 
     <canvas ref="canvasRef" width="640" height="480" class="hidden"></canvas>
@@ -64,10 +82,22 @@ onBeforeUnmount(stopWebcam);
     </div>
 
     <div class="w-full h-80 overflow-y-auto mt-4">
+
+      <div class="flex justify-center pb-4">
+        <UButton v-if="snapshots.length" color="warning" @click="downloadAllSnapshots">Download snapshots</UButton>
+      </div>
+
       <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-        <img
-            v-for="(img, index) in snapshots" :key="index" :src="img"
-            class="w-full h-32 object-cover border rounded-lg shadow-md" alt="snapshot">
+        <div v-for="(img, index) in snapshots" :key="index" class="relative">
+          <img
+              :src="img"
+              class="w-full h-32 object-cover border rounded-lg shadow-md"
+              alt="snapshot"
+          >
+          <a :href="img" download="snapshot.png" class="absolute bottom-2 right-2">
+            <UButton icon="i-lucide-download" variant="subtle" color="primary" class="p-2"/>
+          </a>
+        </div>
       </div>
     </div>
   </div>
